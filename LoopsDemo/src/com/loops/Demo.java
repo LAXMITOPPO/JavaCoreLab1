0f579d68c1cf7fe62ca5e957630d59fc82f71c59
laxmi toppo <laxmi toppo@DESKTOP-CQ3IDJF>
2022-11-06 20:03:22 +0530

    ok

---
+++ LoopsDemo/src/com/loops/Demo.java
@@ -0,0 +1,26 @@
+/*
+ * This is demo class to test package
+ * @author : Laxmi Toppo
+ * @Date : 25oct
+ */
+//create package;
+package com.loops;
+
+//declaring class
+public class Demo {
+	
+	//declaring a instance variable
+	int myValue = 15;
+	
+	//method to print value
+	void printValue()
+	{
+		System.out.println("Value is :"+myValue);
+	}
+	
+	//static method to print message
+    static void printMessage()
+    {
+    	System.out.println("Welcome");
+    }
+}//end of class
---
+++ LoopsDemo/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author laxmi toppo
+ *
+ */
+module LoopsDemo {
+}
